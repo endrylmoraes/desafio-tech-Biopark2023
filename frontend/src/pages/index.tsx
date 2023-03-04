838602fc959d192017697f9b71c13ed51757f1da
Endryl Pereira Moraes <3ndryl@gmail.com>
2023-03-04 10:42:53 -0300

    Criacao de componentes reutilizaveis de input e button e criacao do layout da pagina de login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,47 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/home.module.scss";
+
+import logoImg from "../../public/biopark-logo.svg";
+
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+
 export default function Home() {
   return (
-    <h1>
-      Hello Biopark!
-    </h1>
+    <>
+      <Head>
+        <title>Biopark - Login</title>
+      </Head>
+
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Biopark" />
+        
+        <div className={styles.login}>
+          <form>
+            <Input 
+              placeholder="Digite seu email"
+              type="text"
+            />
+
+            <Input 
+              placeholder="Digite sua senha"
+              type="password"
+            />
+
+            <Button
+              type="submit"
+              loading={false}
+            >
+              Acessar
+            </Button>
+            
+            <a className={styles.text}>
+                Não possui uma conta? Cadastre-se!
+            </a>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
