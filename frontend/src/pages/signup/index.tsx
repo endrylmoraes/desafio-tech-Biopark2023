cc239bb3c4e9dcf7349e18a0c567c3876a0661e8
Endryl Pereira Moraes <3ndryl@gmail.com>
2023-03-04 15:17:57 -0300

    Criado o contexto de autenticacao. Funcionalidades de login, logout e resistrar. Criacao da conexao com a api.

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,55 +1,102 @@
+import { useState, FormEvent, useContext } from "react";
+
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../../../styles/home.module.scss";
 
-import logoImg from "../../../public/biopark-logo.svg";
+import logoImg from "../../../public/logo.svg";
 
 import { Input } from "../../components/ui/input";
 import { Button } from "../../components/ui/button";
 
+import { AuthContext } from "../../contexts/AuthContext";
+
+import { toast } from "react-toastify";
+
 import Link from "next/link";
 
 export default function SignUp() {
+  const { signUp } = useContext(AuthContext);
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  function handleChange(event){
+    const { name, value } = event.target;
+    
+    setUser((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
+  }
+
+  async function handleSignUp(event: FormEvent) {
+    event.preventDefault();
+    
+    if (user.name === "" || user.email === "" || user.password === "") {
+      toast.warn("Você deve preencher todos os campos!");
+      return;
+    }
+
+    setLoading(true);
+
+    await signUp(user);
+
+    setLoading(false);
+  }
+
   return (
     <>
       <Head>
         <title>Biopark - Cadastro</title>
       </Head>
-
-      <div className={styles.containerCenter}>  
+      <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Biopark" />
-        
-        <div className={styles.login}>
-          <h1>Criando sua conta</h1>
 
-          <form>
+        <div className={styles.login}>
+            <h1>Criando sua conta</h1>
+
+          <form onSubmit={handleSignUp}>
             <Input 
               placeholder="Digite seu nome"
               type="text"
+              name="name"
+              value={user.name}
+              onChange={handleChange}
             />
 
             <Input 
               placeholder="Digite seu email"
               type="text"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
             />
 
             <Input 
               placeholder="Digite sua senha"
               type="password"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
             />
 
             <Button
               type="submit"
-              loading={false}
+              loading={loading}
             >
-              Cadastrar
+              Acessar
             </Button>
-
-            <Link href="/" className={styles.text}>
-              Já possui uma conta? Faça login!
-            </Link>
-            
           </form>
+          
+          <Link href="/" className={styles.text}>
+              Já possui uma conta? Faça Login!
+          </Link>
+
         </div>
       </div>
     </>
