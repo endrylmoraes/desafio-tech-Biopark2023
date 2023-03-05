e31284381f3b2522c4c9d2106491f0c223ce84cf
Endryl Pereira Moraes <3ndryl@gmail.com>
2023-03-05 00:12:17 -0300

    Add listagem de edificios

--- frontend/src/pages/building/index.tsx
+++ frontend/src/pages/building/index.tsx
@@ -1,7 +1,95 @@
+import { useState } from "react";
+
+import Head from "next/head";
+
+import { Header } from "@/components/Header";
+import { Button } from "@/components/ui/button";
+
 import styles from "./styles.module.scss";
 
-export default function Apartment(){
+import { setupAPIClient } from "@/services/api";
+
+import { canSSRAuth } from "@/utils/canSSRAuth";
+
+type BuildingProps = {
+  id: string;
+  number: number | string;
+}
+
+interface PageProps{
+  buildings: BuildingProps[];
+}
+
+export default function Apartment({ buildings }: PageProps){
+  const [buildingsList, setBuildingsList] = useState(buildings || []);
+
+  async function getBuildings(){
+    const apiClient = setupAPIClient();
+
+    const response = await apiClient.get("/buildings");
+
+    setBuildingsList(response.data);
+  }
+
   return(
-    <h1>Edifícios</h1>
+    <>
+      <Head>
+        <title>Biopark - Território</title>
+      </Head>
+      <div>
+        <Header/>
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Edifícios</h1>
+            <Button>ADD</Button>
+          </div>
+
+
+
+
+          <article className={styles.listBuildings}>
+          {
+            buildingsList.length === 0 && (
+              <span className={styles.emptyList}>Nenhum edifício cadastrado...</span>
+            )
+          }
+
+          {
+            buildingsList.map( building => (
+              <section key={building.id} className={styles.listItem}>
+                <button>
+                  <div className={styles.tag}></div>
+                  <span>
+                    Edifício {building.number}
+                  </span>
+                </button>
+              </section>
+            ))
+
+          }
+          </article>
+
+
+
+        </main>
+
+      </div>
+    </>
   )
 }
+
+
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+
+  const response = await apiClient.get("/buildings");
+
+  //console.log(response.data);
+  
+  return{
+      props:{
+        buildings: response.data
+      }
+  }
+});
